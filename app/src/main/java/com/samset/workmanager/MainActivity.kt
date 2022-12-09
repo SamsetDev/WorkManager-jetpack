c7d0e048cee0624cd2404c6365a08875e569b44d
Sanjay Singh <a_sanjay.singh@airtelbank.com>
2022-12-10 03:30:35 +0530

    Latest commit

--- app/src/main/java/com/samset/workmanager/MainActivity.kt
+++ app/src/main/java/com/samset/workmanager/MainActivity.kt
@@ -2,40 +2,71 @@ package com.samset.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.lifecycle.Observer
-import androidx.work.Constraints
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
+import androidx.work.*
 import com.samset.workmanager.work.MyWorkManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
+
+    private var TAG: String = MainActivity::class.java.simpleName
+
+    private lateinit var workManager: WorkManager
+    private lateinit var timeWorkRequest: OneTimeWorkRequest
+    private lateinit var periodicWorkRequest: PeriodicWorkRequest
+
+    private var PERIODIC_REQUEST_TAG: String = "periodic_request"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val constraints = Constraints.Builder()
-            .setRequiresCharging(true)
-            .build()
-
-        var request = OneTimeWorkRequestBuilder<MyWorkManager>()
-            .setConstraints(constraints)
-            .build()
+        workManager = WorkManager.getInstance()!!
+        btnstart.setOnClickListener(this)
+        btnstop.setOnClickListener(this)
 
 
-        btnStart.setOnClickListener {
+    }
 
-            WorkManager.getInstance(this).enqueue(request)
+    private fun setupOneTimeWorker() {
+        timeWorkRequest = OneTimeWorkRequest.Builder(MyWorkManager::class.java).build()
+        workManager.enqueue(timeWorkRequest)
+    }
+
+    private fun setupPeriodicWorker() {
+
+        val work = PeriodicWorkRequest.Builder(MyWorkManager::class.java, PeriodicWorkRequest.MIN_PERIODIC_INTERVAL_MILLIS, TimeUnit.MINUTES, PeriodicWorkRequest.MIN_PERIODIC_FLEX_MILLIS, TimeUnit.MINUTES)
+        periodicWorkRequest = work.build()
+
+        workManager.enqueue(periodicWorkRequest)
+
+    }
+
+
+    private fun isScheduleWork(tag: String): Boolean {
+        val statuses = workManager.getWorkInfosByTag(tag)
+        if (statuses.get().isEmpty()) return false
+        var running = false
+
+        for (state in statuses.get()) {
+
+            running = state.state == WorkInfo.State.RUNNING || state.state == WorkInfo.State.ENQUEUED
         }
+        return running
 
-        WorkManager.getInstance(this).getWorkInfoByIdLiveData(request.id)
-            .observe(this, Observer {
+    }
 
-                val status: String = it.state.name
-                Toast.makeText(this,status, Toast.LENGTH_SHORT).show()
-            })
 
+    override fun onClick(view: View?) {
+        if (view == btnstart) {
+            setupOneTimeWorker()
+        } else if (view == btnstop) {
+            !isScheduleWork(PERIODIC_REQUEST_TAG)
+            setupPeriodicWorker()
+
+        }
 
     }
 }
